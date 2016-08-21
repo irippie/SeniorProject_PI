50072cce48d1e51432bfbefdd4ed120869fb9375
kbradley91 <kbradley91@ufl.edu>
2016-08-21 19:58:20 -0400

    Testing, attention please.

---
+++ Debug/kyle_test.d
@@ -0,0 +1,89 @@
+# FIXED
+
+kyle_test.obj: ../kyle_test.c
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/driverlib.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/adc14.h
+kyle_test.obj: D:/TI/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/msp.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/msp432p401r.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/msp_compatibility.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/msp432p401r_classic.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h
+kyle_test.obj: D:/TI/ccsv6/ccs_base/arm/include/system_msp432p401r.h
+kyle_test.obj: D:/TI/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/aes256.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/comp_e.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/cpu.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/crc32.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/cs.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/dma.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/eusci.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/flash.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/fpu.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/gpio.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/i2c.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/interrupt.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/mpu.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/pcm.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/pmap.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/pss.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/ref_a.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/reset.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/rom.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/rom_map.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/rtc_c.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/spi.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/sysctl.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/systick.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/timer32.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/timer_a.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/uart.h
+kyle_test.obj: D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/wdt_a.h
+
+../kyle_test.c: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/driverlib.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/adc14.h: 
+D:/TI/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h: 
+D:/TI/ccsv6/ccs_base/arm/include/msp.h: 
+D:/TI/ccsv6/ccs_base/arm/include/msp432p401r.h: 
+D:/TI/ccsv6/ccs_base/arm/include/msp_compatibility.h: 
+D:/TI/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+D:/TI/ccsv6/ccs_base/arm/include/msp432p401r_classic.h: 
+D:/TI/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h: 
+D:/TI/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h: 
+D:/TI/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h: 
+D:/TI/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h: 
+D:/TI/ccsv6/ccs_base/arm/include/system_msp432p401r.h: 
+D:/TI/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/aes256.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/comp_e.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/cpu.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/crc32.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/cs.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/dma.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/eusci.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/flash.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/fpu.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/gpio.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/i2c.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/interrupt.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/mpu.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/pcm.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/pmap.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/pss.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/ref_a.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/reset.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/rom.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/rom_map.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/rtc_c.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/spi.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/sysctl.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/systick.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/timer32.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/timer_a.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/uart.h: 
+D:/TI/msp432_driverlib_3_21_00_05/driverlib/MSP432P4xx/wdt_a.h: 
---
+++ kyle_test.c
@@ -0,0 +1,82 @@
+//*****************************************************************************
+//
+// MSP432 main.c template - Empty main
+//
+//****************************************************************************
+
+#include "driverlib.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+
+
+
+const eUSCI_UART_Config uartConfig =
+{
+    EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+    78,                                     // BRDIV = 78
+    2,                                       // UCxBRF = 2
+    0,                                       // UCxBRS = 0
+    EUSCI_A_UART_NO_PARITY,                  // No Parity
+    EUSCI_A_UART_LSB_FIRST,                  // MSB First
+    EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+    EUSCI_A_UART_MODE,                       // UART mode
+    EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
+};
+
+int main(void)
+{
+    /* Halting WDT  */
+    MAP_WDT_A_holdTimer();
+
+    /* Selecting P1.2 and P1.3 in UART mode */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+            GPIO_PIN1 | GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Setting DCO to 12MHz */
+    MAP_CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
+
+    /* Configuring UART Module */
+    MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+
+    /* Enable UART module */
+    MAP_UART_enableModule(EUSCI_A0_BASE);
+//    MAP_UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT);
+//    MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
+//    MAP_Interrupt_enableSleepOnIsrExit();
+//
+//    MAP_Interrupt_enableMaster();
+
+
+    while(1){
+
+    	UART_send_string("test");
+
+    	MAP_UART_transmitData(EUSCI_A0_BASE, 0x0D);
+    	MAP_UART_transmitData(EUSCI_A0_BASE, 0x0A); //creates new line
+
+//    	UART_clear_screen();
+//    	MAP_PCM_gotoLPM0();
+    }
+}
+void EUSCIA0_IRQHandler(void)
+{
+    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
+
+    MAP_UART_clearInterruptFlag(EUSCI_A0_BASE, status);
+
+    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
+    {
+        MAP_UART_transmitData(EUSCI_A0_BASE, 0x31);
+    }
+
+}
+void UART_send_string(char* in){
+	int i;
+	for(i = 0; i < strlen(in); i++){
+		MAP_UART_transmitData(EUSCI_A0_BASE, in[i]);
+	}
+}
+void UART_clear_screen(){
+	MAP_UART_transmitData(EUSCI_A0_BASE, 0x1b);
+}
