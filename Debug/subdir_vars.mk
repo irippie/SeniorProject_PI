a11ed9987cfd23a72081ed9cf4abe12fb19d9c02
irippie <jesus51593@live.com>
2016-08-22 01:34:09 -0400

    Dont know why i have to do this again

--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -7,32 +7,32 @@ CMD_SRCS += \
 ../msp432p401r.cmd 
 
 C_SRCS += \
-../kyle_test.c \
+../main.c \
 ../startup_msp432p401r_ccs.c \
 ../system_msp432p401r.c 
 
 OBJS += \
-./kyle_test.obj \
+./main.obj \
 ./startup_msp432p401r_ccs.obj \
 ./system_msp432p401r.obj 
 
 C_DEPS += \
-./kyle_test.d \
+./main.d \
 ./startup_msp432p401r_ccs.d \
 ./system_msp432p401r.d 
 
 C_DEPS__QUOTED += \
-"kyle_test.d" \
+"main.d" \
 "startup_msp432p401r_ccs.d" \
 "system_msp432p401r.d" 
 
 OBJS__QUOTED += \
-"kyle_test.obj" \
+"main.obj" \
 "startup_msp432p401r_ccs.obj" \
 "system_msp432p401r.obj" 
 
 C_SRCS__QUOTED += \
-"../kyle_test.c" \
+"../main.c" \
 "../startup_msp432p401r_ccs.c" \
 "../system_msp432p401r.c" 
 
