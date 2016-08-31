3fc0f0752118ec0282b74d4b687cbf59b41775c5
irippie <jesus51593@live.com>
2016-08-31 11:34:15 -0400

    Working Xbee code. Sends data to Xbee via UART from MSP and Xbee sends it to coordinator.

--- main.c
+++ main.c
@@ -9,15 +9,18 @@
 
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 
+void init_clock();
+void init_uart();
 void UART_transmit_data(const char* data);
 
 const eUSCI_UART_Config uartConfig =
 {
     EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
-    26,                                     // BRDIV = 26
+    13,                                     // BRDIV = 26
     0,                                       // UCxBRF = 0
-    111,                                       // UCxBRS = 111
+    37,                                       // UCxBRS = 111
     EUSCI_A_UART_NO_PARITY,                  // No Parity
     EUSCI_A_UART_LSB_FIRST,                  // MSB First
     EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
@@ -27,43 +30,59 @@ const eUSCI_UART_Config uartConfig =
 
 int main(void)
 {
-    /* Halting WDT  */
-    MAP_WDT_A_holdTimer();
-
-    /* Enable floating point unit to set DCO frequency */
-    MAP_FPU_enableModule();
-
-    MAP_FlashCtl_setWaitState(FLASH_BANK0, 2);
-    MAP_FlashCtl_setWaitState(FLASH_BANK1, 2);
-
-    /* Increasing core voltage to handle higher frequencies */
-    MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
-
-    /* Setting DCO to 48MHz */
-    MAP_CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_48);
-
-    /* Setting P4.3 to output MCLK frequency */
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P4, GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
-
-    /* Selecting P1.2 and P1.3 in UART mode */
-	MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
-			GPIO_PIN1 | GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
-
-	MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
-	MAP_UART_enableModule(EUSCI_A0_BASE);
+	init_clock();
+	init_uart();
 
     while(1)
     {
-    	UART_transmit_data("test");
+    	UART_transmit_data("testing");
     }
 }
 
 void UART_transmit_data(const char* data){
 
 	int i;
-	for(i = 0; i < strlen(data); i++){ MAP_UART_transmitData(EUSCI_A0_BASE, data[i]); }
+	for(i = 0; i < strlen(data); i++){ MAP_UART_transmitData(EUSCI_A2_BASE, data[i]); }
+	MAP_UART_transmitData(EUSCI_A2_BASE, '\r');
 
-	MAP_UART_transmitData(EUSCI_A0_BASE, '\r');
-	MAP_UART_transmitData(EUSCI_A0_BASE, '\n');
+//	for(i = 0; i < strlen(data); i++){ MAP_UART_transmitData(EUSCI_A0_BASE, data[i]); }
+//	MAP_UART_transmitData(EUSCI_A0_BASE, '\r');
+
+}
+
+void init_uart(){
+
+	/* Selecting P3.2 and P3.3 in UART mode */
+	MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3,
+			GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+	MAP_UART_initModule(EUSCI_A2_BASE, &uartConfig);
+	MAP_UART_enableModule(EUSCI_A2_BASE);
+
+//	/* Selecting P1.2 and P1.3 in UART mode */
+//	MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+//			GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+//
+//	MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+//	MAP_UART_enableModule(EUSCI_A0_BASE);
+
+}
+
+void init_clock(){
+
+	/* Halting WDT  */
+	MAP_WDT_A_holdTimer();
+
+	/* Enable floating point unit to set DCO frequency */
+	MAP_FPU_enableModule();
+
+	MAP_FlashCtl_setWaitState(FLASH_BANK0, 2);
+	MAP_FlashCtl_setWaitState(FLASH_BANK1, 2);
+
+	/* Increasing core voltage to handle higher frequencies */
+	MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
+
+	/* Setting DCO to 24MHz */
+	MAP_CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);
 
 }
